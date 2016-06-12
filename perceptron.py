3e2edd398132c6f3b175d0f5a035ca6674f11d9b
Mattias Ljungmark <mattias@ljungmark.co>
2016-06-12 16:18:23 +0200

    Declare training outputs

--- perceptron.py
+++ perceptron.py
@@ -7,3 +7,5 @@ training_inputs = np.array([[0,0,1],
                             [1,1,1],
                             [1,0,1],
                             [0,1,1]])
+
+training_outputs = np.array([[0,1,1,0]]).T
