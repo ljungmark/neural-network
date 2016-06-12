90c8d405e3f5f511b7bf2da1b20df0a470c07303
Mattias Ljungmark <mattias@ljungmark.co>
2016-06-12 14:59:19 +0200

    Set training inputs

--- perceptron.py
+++ perceptron.py
@@ -2,3 +2,8 @@ import numpy as np
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
+training_inputs = np.array([[0,0,1],
+                            [1,1,1],
+                            [1,0,1],
+                            [0,1,1]])
