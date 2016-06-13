936a09d67166c33b43f8b0dde57b8b960f99049f
Mattias Ljungmark <mattias@ljungmark.co>
2016-06-13 12:04:12 +0200

    Synaptic weights

--- perceptron.py
+++ perceptron.py
@@ -9,3 +9,10 @@ training_inputs = np.array([[0,0,1],
                             [0,1,1]])
 
 training_outputs = np.array([[0,1,1,0]]).T
+
+np.random.seed(1)
+
+synaptic_weights = 2 * np.random.random((3, 1)) - 1
+
+print('Random starting synaptic weights: ')
+print(synaptic_weights)
