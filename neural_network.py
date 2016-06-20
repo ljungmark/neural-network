6018fcde51ee9c62ea4a0cca1439b9ae9348b62f
Mattias Ljungmark <mattias@ljungmark.co>
2016-06-20 14:24:23 +0200

    Add user input to test training results

--- neural_network.py
+++ neural_network.py
@@ -8,7 +8,7 @@ class NeuralNetwork():
     def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
 
-    def sigmoid_derivative(x):
+    def sigmoid_derivative(self, x):
         return x * (1 - x)
 
     def train(self, training_inputs, training_outputs, training_iterations):
@@ -19,7 +19,32 @@ class NeuralNetwork():
             self.synaptic_weights += adjustments
 
     def think(self, inputs):
-        inputs = input.setType(float)
-        output = sigmoid(np.dot(inputs, self.synaptic_weights))
+        inputs = inputs.astype(float)
+        output = self.sigmoid(np.dot(inputs, self.synaptic_weights))
 
         return output
+
+if __name__ == "__main__":
+    neural_network = NeuralNetwork()
+    print('Random synpatic weights: ')
+    print(neural_network.synaptic_weights)
+
+    training_inputs = np.array([[0,0,1],
+                                [1,1,1],
+                                [1,0,1],
+                                [0,1,1]])
+
+    training_outputs = np.array([[0,1,1,0]]).T
+
+    neural_network.train(training_inputs, training_outputs, 100000)
+
+    print('Synaptic weights after training: ')
+    print(neural_network.synaptic_weights)
+
+    A = str(input('Input 1: '))
+    B = str(input('Input 2: '))
+    C = str(input('Input 3: '))
+
+    print('New situation: input data: = ', A, B, C)
+    print('Output data: ')
+    print(neural_network.think(np.array([A, B, C])))
